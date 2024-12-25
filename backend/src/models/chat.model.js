41efb5882b8b90c7d336da953b58561d0c6cfa51
Soumya Swaraj Sahoo <85989857+soumya-swaraj@users.noreply.github.com>
2024-12-25 15:42:12 +0530

    new: chat router

---
+++ backend/src/models/chat.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  chatName: {
+    type: String,
+  },
+  admins: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: [],
+    },
+  ],
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  ],
+  isGroupChat: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const Chat = mongoose.model("Chat", schema);
+
+module.exports = { Chat };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 const http = require("http");
 const express = require("express");
 const dotenv = require("dotenv");
-const userRouter = require("./routes/user.route");
+const userRouter = require("./routes/user.route.js");
 const cookieParser = require("cookie-parser");
+const chatRouter = require("./routes/chat.route.js");
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ require("./db");
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/chat", chatRouter);
 
 httpServer.listen(process.env.PORT, () => {
   console.log(`Server is running on ${process.env.PORT}`);
